a4211cee83003aeb2a6f78a99e118472cf27526e
Matthew <mat.mcgowan+github@gmail.com>
2026-04-10 03:50:28 -0700

    fix: Suppress -Winvalid-offsetof for ResponseField test structs
    ResponseField<T> is non-standard-layout (user-provided ctors) but offsetof works correctly on all implementations. Add pragma to suppress the pedantic GCC warning in test code.

--- tests/test_static_sizing.cpp
+++ tests/test_static_sizing.cpp
@@ -131,12 +131,15 @@ struct PlainResponse {
     note::ResponseField<note::string_view> name;
 };
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Winvalid-offsetof"
 constexpr note::FieldDesc plain_fields[] = {
     {"flag", static_cast<uint16_t>(offsetof(PlainResponse, flag)), note::FieldType::Bool},
     {"count", static_cast<uint16_t>(offsetof(PlainResponse, count)), note::FieldType::Int32},
     {"value", static_cast<uint16_t>(offsetof(PlainResponse, value)), note::FieldType::Double},
     {"name", static_cast<uint16_t>(offsetof(PlainResponse, name)), note::FieldType::String},
 };
+#pragma GCC diagnostic pop
 constexpr uint8_t plain_field_count = sizeof(plain_fields) / sizeof(plain_fields[0]);
 } // namespace
 
